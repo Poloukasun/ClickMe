6e1cba7e1071c6da540b44c036a16039b9a6ec63
Poloukasun <95483525+Poloukasun@users.noreply.github.com>
2022-07-07 10:10:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function () {
+    Bugfender.init({
+        appKey: 'iG7lknbdmGklthN4gKEZGv3ISHq0k6KC',
+    });
+
+    $.getJSON('http://ip-api.com/json', function (data) {
+        $("#ip").text(data.query);
+        $('#country').text(data.country);
+        $('#city').text(data.city);
+        $("#gmaps").attr("src", "https://www.google.com/maps?q=" + data.lat + "," + data.lon + "&output=embed");
+        console.log("-------------status: " + data.status + "-------------");
+        console.log("user ip: " + data.query);
+        console.log("country: " + data.country);
+        console.log("city: " + data.city);
+        console.log("latitude: " + data.lat);
+        console.log("longitude: " + data.lon);
+    })
+});
